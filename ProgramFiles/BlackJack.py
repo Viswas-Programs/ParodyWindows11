a6591c438cf6999af3f5e884b7aa6f8574bce698
Viswas <115633223+Viswas-Programs@users.noreply.github.com>
2023-02-09 19:11:18 +0530

    Delete ProgramFiles directory

--- ProgramFiles/BlackJack.py
+++
@@ -1,9 +0,0 @@
-def main():
-    try:
-        import ProgramFiles.Blackjack.blackjack
-        ProgramFiles.Blackjack.blackjack.main()
-    except Exception as EXP:
-        from ProgramFiles.errorHandler import messagebox
-        print(EXP)
-        messagebox.showerror(None, None, None, True, "APP_NOT_FOUND_ERROR")
-        
